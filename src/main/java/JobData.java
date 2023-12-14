61d39f251e1d64e69020754c63fcd5f58572ced3
Aether1a <olkyrah@outlook.com>
2023-12-14 16:45:16 -0600

    task 2 done

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -95,7 +95,19 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            for (String aValue : row.values()) {
+//                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+                    if (!jobs.contains(row)) {
+                        jobs.add(row);
+                    }
+//                }
+            }
+        }
+        return jobs;
     }
 
     /**
